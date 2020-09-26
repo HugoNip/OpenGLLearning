fa4190d628f60f81e9bd435d0afdd9fb61674128
HugoNip <gnie@ncsu.edu>
2020-09-26 18:03:59 -0400

    tt3

---
+++ tt03_Matrices/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM (OpenGL Mathematics)
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp> // for transformation
+using namespace glm;
+
+#include <common/shader.hpp>
+
+int main(void)
+{
+    
+
+    return 0;
+}
